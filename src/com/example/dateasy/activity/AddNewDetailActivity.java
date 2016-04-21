8f511105fa5f1d0237e89ec682d76e4ac069c74e
Xu <280745397@qq.com>
2016-04-21 15:37:57 +0800

    增加一些常量

---
+++ src/com/example/dateasy/activity/AddNewDetailActivity.java
@@ -0,0 +1,23 @@
+package com.example.dateasy.activity;
+
+import com.example.dateasy.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AddNewDetailActivity extends Activity {
+
+	private TextView mTitleTextView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_activity_detail);
+		
+		
+		
+	}
+	
+}
---
+++ src/com/example/dateasy/consts/Const.java
@@ -0,0 +1,17 @@
+package com.example.dateasy.consts;
+
+public class Const {
+
+	public static String OUTDOOR_ACTIVITIES = "户外活动";
+	
+	public static String GATHERING = "组织聚会";
+	
+	public static String MEETING = "行业会议";
+	
+	public static String ENTERTAINMENT = "文娱活动";
+	
+	public static String ACTIVITY_SIGNUP = "活动报名";
+	
+	public static String OTHERS = "自定义活动";
+	
+}
