9b51b8a4bdad873dae67d30c9e39b2b1445d235d
Xu <280745397@qq.com>
2016-05-02 23:16:36 +0800

    增加登录与注册

---
+++ src/com/example/dateasy/activity/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.dateasy.activity;
+
+/**
+ * 登录界面
+ * @author Xu
+ * 
+ */
+import com.example.dateasy.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_xml);
+	}
+	
+}
---
+++ src/com/example/dateasy/activity/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.dateasy.activity;
+/**
+ * 注册Activity
+ * @author Xu
+ * 
+ */
+import com.example.dateasy.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register_xml);
+	}
+
+}
