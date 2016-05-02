819f4c004d3083b11570ddfa189d84f691e2c03b
Xu <280745397@qq.com>
2016-05-02 23:52:30 +0800

    登录注册(2)

--- src/com/example/dateasy/activity/LoginActivity.java
+++ src/com/example/dateasy/activity/LoginActivity.java
@@ -5,18 +5,49 @@ package com.example.dateasy.activity;
  * @author Xu
  * 
  */
-import com.example.dateasy.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class LoginActivity extends Activity {
+import com.example.dateasy.R;
+import com.example.dateasy.util.Utils;
 
+public class LoginActivity extends Activity implements OnClickListener{
+
+	private Button mRegisterButton;
+	private Button mLoginButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_xml);
+		initViews();
+	}
+
+	private void initViews() {
+		// TODO Auto-generated method stub
+		mRegisterButton = (Button) findViewById(R.id.register_bt);
+		mLoginButton = (Button) findViewById(R.id.login_bt);
+		mRegisterButton.setOnClickListener(this);
+		mLoginButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.register_bt:
+			Utils.toAnotherActivity(LoginActivity.this, RegisterActivity.class);
+			break;
+
+		case R.id.login_bt:
+			Utils.toAnotherActivity(LoginActivity.this, MainActivity.class);
+			finish();
+			break;
+		}
 	}
 	
 }
--- src/com/example/dateasy/activity/RegisterActivity.java
+++ src/com/example/dateasy/activity/RegisterActivity.java
@@ -4,18 +4,56 @@ package com.example.dateasy.activity;
  * @author Xu
  * 
  */
-import com.example.dateasy.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class RegisterActivity extends Activity {
+import com.example.dateasy.R;
+import com.example.dateasy.util.Utils;
 
+public class RegisterActivity extends Activity implements OnClickListener{
+
+	private ImageButton mBackImageButton;
+	private Button mLoginButton;
+	private Button mRegisterAndLoginButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register_xml);
+		initViews();
+	}
+
+	private void initViews() {
+		// TODO Auto-generated method stub
+		mBackImageButton = (ImageButton) findViewById(R.id.register_back_ib);
+		mLoginButton = (Button) findViewById(R.id.register_login_bt);
+		mRegisterAndLoginButton = (Button) findViewById(R.id.register_and_login_bt);
+		mBackImageButton.setOnClickListener(this);
+		mLoginButton.setOnClickListener(this);
+		mRegisterAndLoginButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.register_back_ib:
+			finish();
+			break;
+
+		case R.id.register_login_bt:
+			Utils.toAnotherActivity(RegisterActivity.this, MainActivity.class);
+			break;
+			
+		case R.id.register_and_login_bt:
+			Utils.toAnotherActivity(RegisterActivity.this, MainActivity.class);
+			break;
+		}
 	}
 
 }
